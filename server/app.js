f009859c334bea18d70c7eec7a23728b50f96c88
michael-lfc <michelino.realmike@gmail.com>
2025-11-20 14:58:19 +0100

    new commit

--- server/app.js
+++ server/app.js
@@ -14,6 +14,11 @@ const PORT = process.env.PORT || 4000;
 app.use(cors());
 app.use(express.json());
 
+// Root route for testing
+app.get("/", (req, res) => {
+  res.send("Backend is running!");
+});
+
 // Routes
 app.use("/users", userRouter);
 app.use("/recipes", recipeRouter);
